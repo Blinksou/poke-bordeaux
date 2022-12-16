61b6e0f19bbe1f872d154caae81157f8b1ea3da1
Blinksou <onair921@gmail.com>
2022-12-16 14:23:20 +0100

    chore: config app

---
+++ apps/app/src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'poke-bordeaux',
+    appId: '1:622454267580:web:7507ff5032dfb45477fab9',
+    storageBucket: 'poke-bordeaux.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyAHAW58ecJctpxzz-CTmLtEFEd08tAQ6VI',
+    authDomain: 'poke-bordeaux.firebaseapp.com',
+    messagingSenderId: '622454267580',
+  },}
--- apps/app/src/main.ts
+++ apps/app/src/main.ts
@@ -1,11 +1,30 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import {
-  provideRouter,
-  withEnabledBlockingInitialNavigation,
-} from '@angular/router';
-import { AppComponent } from './app/app.component';
-import { appRoutes } from './app/app.routes';
+import { bootstrapApplication } from "@angular/platform-browser";
+import { provideRouter, withEnabledBlockingInitialNavigation } from "@angular/router";
+import { AppComponent } from "./app/app.component";
+import { appRoutes } from "./app/app.routes";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { importProvidersFrom, isDevMode } from "@angular/core";
+import { initializeApp, provideFirebaseApp } from "@angular/fire/app";
+import { environment } from "./environments/environment";
+import { getAuth, provideAuth } from "@angular/fire/auth";
+import { getFirestore, provideFirestore } from "@angular/fire/firestore";
+import { getStorage, provideStorage } from "@angular/fire/storage";
+import { ServiceWorkerModule } from "@angular/service-worker";
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(appRoutes, withEnabledBlockingInitialNavigation())],
+  providers: [
+    provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
+    importProvidersFrom(
+      BrowserAnimationsModule,
+      ServiceWorkerModule.register("ngsw-worker.js", {
+        enabled: !isDevMode(),
+        // Register the ServiceWorker as soon as the application is stable
+        // or after 30 seconds (whichever comes first).
+        registrationStrategy: "registerWhenStable:30000"
+      }),
+      provideFirebaseApp(() => initializeApp(environment.firebase)),
+      provideAuth(() => getAuth()),
+      provideFirestore(() => getFirestore()),
+      provideStorage(() => getStorage()))
+  ]
 }).catch((err) => console.error(err));
