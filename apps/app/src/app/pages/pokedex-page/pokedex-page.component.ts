d37179f3972c3aee4e8aecee152b2322ba420fc9
maximelarrieu <maxime.larrieu@ynov.com>
2022-12-16 14:46:44 +0100

    feat: initializa pokedex page

---
+++ apps/app/src/app/pages/pokedex-page/pokedex-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokedex-page',
+  templateUrl: './pokedex-page.component.html',
+  styleUrls: ['./pokedex-page.component.scss'],
+})
+export class PokedexPageComponent {}
