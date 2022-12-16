9d93f6de86e88aa96e4e8d8fe8508d1e1c7570b4
maximelarrieu <maxime.larrieu@ynov.com>
2022-12-16 14:55:10 +0100

    feat: initialize pokedex page

--- apps/app/src/app/app.routes.ts
+++ apps/app/src/app/app.routes.ts
@@ -1,13 +1,33 @@
-import { Route } from "@angular/router";
-import { HomePageComponent } from "./pages/home-page/home-page.component";
+import { Route } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
+import { HuntPageComponent } from './pages/hunt-page/hunt-page.component';
+import { PokedexPageComponent } from './pages/pokedex-page/pokedex-page.component';
 
 export const appRoutes: Route[] = [
   {
-    path: "",
-    component: HomePageComponent
+    path: '',
+    component: HomePageComponent,
   },
   {
-    path: "**",
-    redirectTo: ""
-  }
+    path: 'profile',
+    component: ProfilePageComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterPageComponent,
+  },
+  {
+    path: 'hunt',
+    component: HuntPageComponent,
+  },
+  {
+    path: 'pokedex',
+    component: PokedexPageComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
 ];
