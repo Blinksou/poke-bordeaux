256d315b06c0f660dff4a70f8288705b2872e788
mokobe-ebunike <m.aboulicam@eenov.com>
2022-12-16 14:48:39 +0100

    feat(profile-page): routing
    Signed-off-by: “Ymokobe-ebunike <m.aboulicam@eenov.com>

--- apps/app/src/app/app.routes.ts
+++ apps/app/src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Route } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import {ProfilePageComponent} from "./pages/profile-page/profile-page.component";
 
 export const appRoutes: Route[] = [
   {
     path: '',
     component: HomePageComponent,
+  },
+  {
+    path: 'profile',
+    component: ProfilePageComponent,
   },
   {
     path: 'register',
