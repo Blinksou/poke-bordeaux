0b9dc84e548be4a54a69e786c543a2b40b1ec1e0
Thomas Le Naour <lenaourthomas@gmail.com>
2022-12-16 14:45:05 +0100

    feat(routing): add register page

--- apps/app/src/app/app.routes.ts
+++ apps/app/src/app/app.routes.ts
@@ -1,13 +1,18 @@
-import { Route } from "@angular/router";
-import { HomePageComponent } from "./pages/home-page/home-page.component";
+import { Route } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
 export const appRoutes: Route[] = [
   {
-    path: "",
-    component: HomePageComponent
+    path: '',
+    component: HomePageComponent,
   },
   {
-    path: "**",
-    redirectTo: ""
-  }
+    path: 'register',
+    component: RegisterPageComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
 ];
