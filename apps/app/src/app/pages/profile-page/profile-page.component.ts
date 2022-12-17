6f4b4e40171c47d1539b880162affe503d66f5bb
mokobe-ebunike <m.aboulicam@eenov.com>
2022-12-17 11:51:03 +0100

    feat(profile-page): imports
    Signed-off-by: mokobe-ebunike <m.aboulicam@eenov.com>

--- apps/app/src/app/pages/profile-page/profile-page.component.ts
+++ apps/app/src/app/pages/profile-page/profile-page.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-profile-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatGridListModule, MatSlideToggleModule],
   templateUrl: './profile-page.component.html',
-  styleUrls: ['./profile-page.component.scss']
+  styleUrls: ['./profile-page.component.scss'],
 })
-export class ProfilePageComponent {
-
-}
+export class ProfilePageComponent {}
