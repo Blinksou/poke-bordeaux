7e60990063e15978b848b59694f0cb0f97aa7f59
mokobe-ebunike <m.aboulicam@eenov.com>
2022-12-17 14:38:56 +0100

    feat(profile-page): imports
    Signed-off-by: mokobe-ebunike <m.aboulicam@eenov.com>

--- apps/app/src/app/pages/profile-page/profile-page.component.ts
+++ apps/app/src/app/pages/profile-page/profile-page.component.ts
@@ -2,11 +2,19 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { PokemonAvatarComponent } from '../../components/pokemon-avatar/pokemon-avatar.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-profile-page',
   standalone: true,
-  imports: [CommonModule, MatGridListModule, MatSlideToggleModule],
+  imports: [
+    CommonModule,
+    MatGridListModule,
+    MatSlideToggleModule,
+    PokemonAvatarComponent,
+    MatIconModule,
+  ],
   templateUrl: './profile-page.component.html',
   styleUrls: ['./profile-page.component.scss'],
 })
