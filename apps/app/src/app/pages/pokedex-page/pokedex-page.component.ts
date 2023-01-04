4653fd831a5a548c6f0866a993191a9e8f456d48
maximelarrieu <maxime.larrieu@ynov.com>
2023-01-04 18:23:50 +0100

    feat: initialize pokedex page

--- apps/app/src/app/pages/pokedex-page/pokedex-page.component.ts
+++ apps/app/src/app/pages/pokedex-page/pokedex-page.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Component, Input } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-pokedex-page',
   standalone: true,
+  imports: [CommonModule, FormsModule, MatFormFieldModule, MatInputModule, MatIconModule],
   templateUrl: './pokedex-page.component.html',
   styleUrls: ['./pokedex-page.component.scss'],
 })
-export class PokedexPageComponent {}
+export class PokedexPageComponent {
+  @Input() search: string | undefined;
+}
