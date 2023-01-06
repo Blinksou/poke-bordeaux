df2315b17df827e3f971bc00189fc5e48387fae7
maximelarrieu <contact@maximelarrieu.io>
2023-01-06 11:00:48 +0100

    merge

--- apps/app/src/app/pages/pokedex-page/pokedex-page.component.ts
+++ apps/app/src/app/pages/pokedex-page/pokedex-page.component.ts
@@ -1,9 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+
+import { PokedexPageService } from './pokedex-page.service';
+import { PokeApi } from '../../../interfaces';
 
 @Component({
   selector: 'app-pokedex-page',
@@ -12,6 +15,28 @@ import { MatInputModule } from '@angular/material/input';
   templateUrl: './pokedex-page.component.html',
   styleUrls: ['./pokedex-page.component.scss'],
 })
-export class PokedexPageComponent {
+export class PokedexPageComponent implements OnInit {
+  loaded!: boolean;
+  typesList: [] | undefined;
+  
+  constructor(private pokedexService: PokedexPageService) {}
+
   @Input() search: string | undefined;
+
+  ngOnInit(): void {
+    this.getItems();
+  }
+
+  getItems() {
+    this.loaded = false;
+    this.pokedexService.getTypes('https://pokeapi.co/api/v2/type')
+      .subscribe(
+        (response: PokeApi) => {
+          console.log('response', response.results)
+          // listtypes.forEach(type =>
+          // this.types = listtypes;
+        }
+      );
+  }
+
 }
--- apps/app/src/main.ts
+++ apps/app/src/main.ts
@@ -11,11 +11,25 @@ import {getFirestore, provideFirestore} from "@angular/fire/firestore";
 import {getStorage, provideStorage} from "@angular/fire/storage";
 import {ServiceWorkerModule} from "@angular/service-worker";
 import {TranslateModule} from "@ngx-translate/core";
+import { bootstrapApplication } from "@angular/platform-browser";
+import { provideRouter, withEnabledBlockingInitialNavigation } from "@angular/router";
+import { AppComponent } from "./app/app.component";
+import { appRoutes } from "./app/app.routes";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { importProvidersFrom, isDevMode } from "@angular/core";
+import { initializeApp, provideFirebaseApp } from "@angular/fire/app";
+import { environment } from "./environments/environment";
+import { getAuth, provideAuth } from "@angular/fire/auth";
+import { getFirestore, provideFirestore } from "@angular/fire/firestore";
+import { getStorage, provideStorage } from "@angular/fire/storage";
+import { ServiceWorkerModule } from "@angular/service-worker";
+import { HttpClientModule } from "@angular/common/http";
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
     importProvidersFrom(
+      HttpClientModule,
       BrowserAnimationsModule,
       TranslateModule.forRoot(),
       ServiceWorkerModule.register("ngsw-worker.js", {
