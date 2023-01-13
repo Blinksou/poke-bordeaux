9840678a0921b58db55b3390991673ccc7c369ea
mokobe-ebunike <marvyn.ap@gmail.com>
2023-01-13 16:52:14 +0100

    feat(ProfilePageTakePhoto): component logic

---
+++ apps/app/src/app/pages/profile-page/profile-page-header/profile-page-take-photo/profile-page-take-photo.component.ts
@@ -0,0 +1,86 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatDialogModule } from '@angular/material/dialog';
+import {
+  WebcamImage,
+  WebcamInitError,
+  WebcamModule,
+  WebcamUtil,
+} from 'ngx-webcam';
+import { Observable, Subject } from 'rxjs';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-profile-page-take-photo',
+  standalone: true,
+  imports: [CommonModule, MatDialogModule, WebcamModule, MatIconModule],
+  templateUrl: './profile-page-take-photo.component.html',
+  styleUrls: ['./profile-page-take-photo.component.scss'],
+})
+export class ProfilePageTakePhotoComponent implements OnInit {
+  @Output()
+  public pictureTaken = new EventEmitter<WebcamImage>();
+  public webcamImage: WebcamImage | undefined;
+  public showWebcam = true;
+  public allowCameraSwitch = true;
+  public multipleWebcamsAvailable = false;
+  public deviceId: string | undefined;
+  public videoOptions: MediaTrackConstraints = {};
+  public errors: WebcamInitError[] = [];
+
+  private trigger: Subject<void> = new Subject<void>();
+  private nextWebcam: Subject<boolean | string> = new Subject<
+    boolean | string
+  >();
+
+  public ngOnInit(): void {
+    WebcamUtil.getAvailableVideoInputs().then(
+      (mediaDevices: MediaDeviceInfo[]) => {
+        this.multipleWebcamsAvailable = mediaDevices && mediaDevices.length > 1;
+      }
+    );
+  }
+
+  public triggerSnapshot(): void {
+    this.trigger.next();
+  }
+
+  public toggleWebcam(): void {
+    this.showWebcam = !this.showWebcam;
+  }
+
+  public retryWebcamImage() {
+    this.webcamImage = undefined;
+  }
+
+  public keepWebcamImage() {
+    return this.webcamImage;
+  }
+
+  public handleInitError(error: WebcamInitError): void {
+    this.errors.push(error);
+  }
+
+  public showNextWebcam(directionOrDeviceId: boolean | string): void {
+    // true => move forward through devices
+    // false => move backwards through devices
+    // string => move to device with given deviceId
+    this.nextWebcam.next(directionOrDeviceId);
+  }
+
+  public handleImage(webcamImage: WebcamImage): void {
+    this.webcamImage = webcamImage;
+  }
+
+  public cameraWasSwitched(deviceId: string): void {
+    this.deviceId = deviceId;
+  }
+
+  public get triggerObservable(): Observable<void> {
+    return this.trigger.asObservable();
+  }
+
+  public get nextWebcamObservable(): Observable<boolean | string> {
+    return this.nextWebcam.asObservable();
+  }
+}
