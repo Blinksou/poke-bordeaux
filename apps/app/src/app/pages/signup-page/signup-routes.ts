ecaa73c979f3f410742420b2c14dc853b08554fb
Blinksou <onair921@gmail.com>
2023-01-19 11:35:38 +0100

    chore(perfs): add route lazy loading

---
+++ apps/app/src/app/pages/signup-page/signup-routes.ts
@@ -0,0 +1,6 @@
+import { Route } from '@angular/router';
+import { SignupPageComponent } from './signup-page.component';
+
+export const signupPageRoutes: Route[] = [
+  { path: '', component: SignupPageComponent },
+];
