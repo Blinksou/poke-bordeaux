33056e9a09f54e52ac80b80ae55c02098a90ba8d
Blinksou <onair921@gmail.com>
2023-01-20 09:16:55 +0100

    chore: change getPokemonFromId method

--- apps/app/src/app/services/pokemon.service.ts
+++ apps/app/src/app/services/pokemon.service.ts
@@ -1,14 +1,13 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 /** DATA */
 import pokemons from '../../assets/pokemon/pokemons-list.json';
 
 /** MODEL */
 import { Pokemon } from '../components/pokemon-avatar/model/pokemon';
+import { of } from 'rxjs';
 
 /** RXJS */
-import { map } from 'rxjs';
 
 const pokemonsArray = Object.values(pokemons);
 
@@ -16,12 +15,8 @@ const pokemonsArray = Object.values(pokemons);
   providedIn: 'root',
 })
 export class PokemonService {
-  constructor(private readonly http: HttpClient) {}
-
-  getPokemonFromId(id: string) {
-    return this.http
-      .get(`https://pokeapi.co/api/v2/pokemon/${id}`)
-      .pipe(map((response) => response as Pokemon));
+  getPokemonFromId(id: number | string) {
+    return of(pokemonsArray[Number(id)] as Pokemon);
   }
 
   getRandomPokemon(): Pokemon {
