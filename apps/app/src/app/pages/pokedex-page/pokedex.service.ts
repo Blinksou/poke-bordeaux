3d2f5399c72fbb4a6fdfb7bf031476faa6fc7ed9
Alex Boisseau <alex.boisseau79@gmail.com>
2023-01-20 14:00:17 +0100

    refactor: move logic about pokedex inside a service

--- apps/app/src/app/pages/pokedex-page/pokedex.service.ts
+++ apps/app/src/app/pages/pokedex-page/pokedex.service.ts
@@ -1,21 +1,66 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+
+/** DATA */
+import PokemonList from '../../../assets/pokemon/pokemons-list.json';
+
+/** MODEL */
 import { Pokemon } from '../../components/pokemon-avatar/model/pokemon';
 
-type Pokedex = Array<{
-    pokemon: Pokemon;
-    userQuantity?: number;
-}>
+/** SERVICES */
+import { UserService } from '../../services/user.service';
 
-@Injectable({
-    providedIn: 'root',
-})
-export class PokedexService {
-    pokedex$: Observable<Pokedex | null> = of(null);
+/** RXJS */
+import { map, Observable, of } from 'rxjs';
+
+export type PokedexPokemon = Pokemon & {quantity?: number};
+type OwnedAndUnownedUserPokemons = {
+  owned: PokedexPokemon[],
+  unowned: PokedexPokemon[]
 }
 
+@Injectable({
+  providedIn: 'root',
+})
+export class PokedexService {
+  pokedexPokemons$: Observable<PokedexPokemon[] | null> = of(null);
 
-/*
+	// this.huntState$ = this.userService.user$.pipe(
+	// 	map((userProfile) => {
+	// 		if (!userProfile) return null;
+			
+	// 		const hunt = userProfile.hunt;
+	// 		const energiesState = this.determineEnergiesState(hunt.energiesDate);
+	// 		const pokeballsState: PokeballsState = this.determinePokeballsState(hunt.pokeballs);
 
-1 - filter sur les pokemons 
-*/
+	// 		return {
+	// 			energiesState,
+	// 			pokeballsState,
+	// 		};
+	// 	})
+	// )
+
+	constructor(
+		readonly userService: UserService
+	) {
+		const pokemonsList = Object.values(PokemonList) as unknown as Pokemon[];
+
+		this.pokedexPokemons$ = this.userService.user$.pipe(
+			map((user) => {
+      	if (!user) return null;
+
+      	const userPokemonsMap = new Map(user.pokemons.map(p => [+p.pokemonId, p]));
+      
+				const temp = pokemonsList.reduce((acc, p) => {
+						if (userPokemonsMap.has(p.id)) {
+							acc.owned.push({...p, quantity: userPokemonsMap.get(p.id)?.quantity});
+						} else {
+							acc.unowned.push(p);
+						}
+
+						return acc;
+					}, {owned: [], unowned: []} as OwnedAndUnownedUserPokemons);
+				
+				return [...temp.owned, ...temp.unowned];
+    }));
+	}
+}
