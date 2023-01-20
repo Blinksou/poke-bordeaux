c39d8467eef79e8be99ce7eea1c42bada1c03a41
Alex Boisseau <alex.boisseau79@gmail.com>
2023-01-20 12:37:02 +0100

    feat: sync user pokemons with json list

--- apps/app/src/app/pages/pokedex-page/pokedex-page.component.ts
+++ apps/app/src/app/pages/pokedex-page/pokedex-page.component.ts
@@ -4,9 +4,6 @@ import {
   ChangeDetectionStrategy,
   Component,
   Input,
-  OnChanges,
-  OnInit,
-  ViewChild,
 } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
@@ -18,14 +15,16 @@ import { PokemonAvatarComponent } from '../../components/pokemon-avatar/pokemon-
 import { Pokemon } from '../../components/pokemon-avatar/model/pokemon';
 import { PokemonType } from '../../components/pokemon-avatar/model/pokemon';
 import PokemonList from '../../../assets/pokemon/pokemons-list.json';
-import { UserProfile } from '../../model/user';
 import { UserService } from '../../services/user.service';
-import { Observable } from 'rxjs';
 import { userPokemon } from '../../model/userPokemon';
 
 const pokemonsList = Object.values(PokemonList) as unknown as Pokemon[];
 
 export type PokedexPokemon = Pokemon & {quantity?: number};
+interface OwnedAndUnownedUserPokemons {
+  owned: PokedexPokemon[],
+  unowned: PokedexPokemon[]
+}
 
 @Component({
   selector: 'app-pokedex-page',
@@ -41,8 +40,7 @@ export type PokedexPokemon = Pokemon & {quantity?: number};
     MatGridListModule,
   ],
   templateUrl: './pokedex-page.component.html',
-  styleUrls: ['./pokedex-page.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrls: ['./pokedex-page.component.scss']
 })
 export class PokedexPageComponent {
   // user$: Observable<UserProfile | null>;
@@ -62,24 +60,19 @@ export class PokedexPageComponent {
     this.userService.user$.subscribe((user) => {
       if (!user) return;
 
-      console.log(user.pokemons);
-      const userPokemonsId = user.pokemons.map(p => p.pokemonId);
-      const userPokemonsOwned = pokemonsList.filter(p => userPokemonsId.includes(p.id)).map(p => {
-        const index = user.pokemons.findIndex(up => up.pokemonId == p.id)
-        
-        return {
-          ...p,
-          quantity: user.pokemons[index].quantity
-        }
-      });
-      const userPokemonsUnowned = pokemonsList.filter(p => !userPokemonsId.includes(p.id));
-    
-      this.pokemons = [
-        ...userPokemonsOwned,
-        ...userPokemonsUnowned
-      ];
+      const userPokemonsMap = new Map(user.pokemons.map(p => [+p.pokemonId, p]));
+      
+      const temp = pokemonsList.reduce((acc, p) => {
+          if (userPokemonsMap.has(p.id)) {
+            acc.owned.push({...p, quantity: userPokemonsMap.get(p.id)?.quantity});
+          } else {
+            acc.unowned.push(p);
+          }
 
-      console.log('this.pokemons', this.pokemons)
+          return acc;
+        }, {owned: [], unowned: []} as OwnedAndUnownedUserPokemons);
+      
+      this.pokemons = [...temp.owned, ...temp.unowned];
     });
   }
 
