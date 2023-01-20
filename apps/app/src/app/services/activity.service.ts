30f3d78aafc5db694e557f4d817fa4243e91cf26
Blinksou <onair921@gmail.com>
2023-01-20 11:23:59 +0100

    chore(activity): refact

--- apps/app/src/app/services/activity.service.ts
+++ apps/app/src/app/services/activity.service.ts
@@ -8,16 +8,36 @@ import { addDoc, collection, Firestore } from '@angular/fire/firestore';
 export class ActivityService {
   constructor(private readonly firestore: Firestore) {}
 
+  private async addActivity(activity: BaseActivity<unknown>) {
+    const activitiesCollection = collection(this.firestore, `activities`);
+
+    return addDoc(activitiesCollection, activity);
+  }
+
   async addCaptureActivity(
     activity: Omit<BaseActivity<CaptureActivityPayload>, 'type'>
   ) {
-    console.log('addCaptureActivity', activity);
-
-    const activitiesCollection = collection(this.firestore, `activities`);
-
-    return addDoc(activitiesCollection, {
+    return this.addActivity({
       ...activity,
       type: 'capture',
+    });
+  }
+
+  async addTradeInfoActivity(
+    activity: Omit<BaseActivity<CaptureActivityPayload>, 'type'>
+  ) {
+    return this.addActivity({
+      ...activity,
+      type: 'trade-info',
+    });
+  }
+
+  async addTradeAskActivity(
+    activity: Omit<BaseActivity<CaptureActivityPayload>, 'type'>
+  ) {
+    return this.addActivity({
+      ...activity,
+      type: 'trade-ask',
     });
   }
 }
