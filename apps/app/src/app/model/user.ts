03fd827c77db7e47fa807f53fba019114887cb7c
maximelarrieu <contact@maximelarrieu.io>
2023-01-26 10:18:41 +0100

    ui(pokedex-tooltip): improve tooltip when pokemon selected

--- apps/app/src/app/model/user.ts
+++ apps/app/src/app/model/user.ts
@@ -22,13 +22,6 @@ export type UserProfile = {
     tradingFulfilled: number;
   };
   hunt: Hunt;
-  pokemons: [
-    {
-      pokemonId: number;
-      quantity: number;
-      isFavorite: boolean;
-    }
-  ]
 };
 
 export type UserBasicInfos = {
