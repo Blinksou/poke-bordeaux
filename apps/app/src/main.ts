1d1cbe6de99952892d4d63e2e507f5233bf65ead
mokobe-ebunike <marvyn.ap@gmail.com>
2023-02-02 12:10:06 +0100

    feat(firestore): offline mode + cache size

--- apps/app/src/main.ts
+++ apps/app/src/main.ts
@@ -7,10 +7,15 @@ import { AppComponent } from './app/app.component';
 import { appRoutes } from './app/app.routes';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { importProvidersFrom, isDevMode } from '@angular/core';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getApp, initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from './environments/environment';
 import { getAuth, provideAuth } from '@angular/fire/auth';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import {
+  enableIndexedDbPersistence,
+  initializeFirestore,
+  provideFirestore,
+  CACHE_SIZE_UNLIMITED,
+} from '@angular/fire/firestore';
 import { getStorage, provideStorage } from '@angular/fire/storage';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { TranslateModule } from '@ngx-translate/core';
@@ -33,7 +38,23 @@ bootstrapApplication(AppComponent, {
       }),
       provideFirebaseApp(() => initializeApp(environment.firebase)),
       provideAuth(() => getAuth()),
-      provideFirestore(() => getFirestore()),
+      provideFirestore(() => {
+        const firestore = initializeFirestore(getApp(), {
+          cacheSizeBytes: CACHE_SIZE_UNLIMITED,
+        });
+        enableIndexedDbPersistence(firestore).catch((err) => {
+          if (err.code == 'failed-precondition') {
+            console.log(
+              'Multiple tabs open, persistence can only be enabled in one tab at a a time.'
+            );
+          } else if (err.code == 'unimplemented') {
+            console.log(
+              'The current browser does not support all of the  features required to enable persistence'
+            );
+          }
+        });
+        return firestore;
+      }),
       provideStorage(() => getStorage())
     ),
   ],
